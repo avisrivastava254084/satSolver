871da4d1fd19d12cc2d070543f35b5539caf6bc9
avisrivastava254084 <avi.srivastava254084@gmail.com>
2017-08-10 12:39:56 +0530

    The following changes are made; 1. The parser is improved, it is now more pythonic. 2. The sat solver is completely built and generating all the solutions. 3. Note: The sat solver is using a well renowned library, pycosat.

--- readingDIMACS.py
+++ readingDIMACS.py
@@ -1,10 +1,8 @@
 import sys, copy
-
-global clauseSet, clauseFileLines, solved
-clauseSet = []
-solved = []
+import pycosat
 
 def readDMACS():
+	global clauseFile, clauseFileLines, clauseSet
 	clauseFile = open('days.txt', "r")
 	clauseFileLines = clauseFile.readlines()
 	clauseSet = list()
@@ -25,97 +23,11 @@ def readDMACS():
 	print(clauseSet)
 	print(maxvar)
 
-def DPLL(set):
-	if(len(set) == 0):	#clause set empty
-		return True	
-	for clause in set:	#contains an empty clause
-		if(len(clause) == 1):	
-			return False	
-	for clause in set:	#unit prop on unit clause
-		index = 0
-		if(len(clause) == 2):
-			item = clause[0]
-			if item < 0:	
-				solved[-1*item] = 0	
-			else:	
-				solved[item] = 1	
-			set.pop(index)
-			index = 0
-			while (index != len(set)):	
-				marked = False	
-				for lit in set[index]:	
-					if lit == item:	
-						marked = True	
-						break	
-					elif (lit == (-1*item)):	
-						set[index].remove(lit)	
-				if marked:	
-					set.pop(index)
-					index = index - 1
-				index = index + 1
-			return DPLL(copy.deepcopy(set))
-	
-	shortClause = set[0]
-	for clause in set: #choose a variable by shortest remaining 
-		if len(shortClause) > len(clause):
-			shortClause = clause
-	item = shortClause[0]
-	tempSet = copy.deepcopy(set)
-	tempSet.remove(shortClause)
-
-	index = 0
-	while (index != len(tempSet)):
-		marked = False
-		for lit in tempSet[index]:
-			if lit == item:
-				marked = True
-				break
-			elif (lit == (-1*item)):
-				tempSet[index].remove(lit)
-		if marked:
-			tempSet.pop(index)
-			index = index - 1
-		index = index + 1
-	tempSolve = copy.deepcopy(solved)
-	done = DPLL(tempSet)
-	if done:
-		if item < 0:
-			solved[-1*item] = 0
-		else:
-			solved[item] = 1
-		return done
-	else:
-		solved = copy.deepcopy(tempSolve)
-		item = (-1*item)
-		tempSet = copy.deepcopy(set)
-		index = 0
-		while (index != len(tempSet)):
-			marked = False
-			for lit in tempSet[index]:
-				if lit == item:
-					marked = True
-					break
-				elif (lit == (-1*item)):
-					tempSet[index].remove(lit)
-			if marked:
-				tempSet.pop(index)
-				index = index -1
-			index = index + 1
-		if item < 0:
-			solved[-1*item] = 0
-		else:
-			solved[item] = 1
-	return DPLL(tempSet)
-
 if __name__ == "__main__":
-	print "Hi, program begins!"
+	print "Welcome!"
 	readDMACS()
-	done = DPLL(copy.deepcopy(clauseSet))
-	DPLL(copy.deepcopy(clauseSet))
-	print "The above statement has been crossed."
-	print "Done has something"
-	for set in solved:
-		print "Solved is not empty"
-		print set, solved[set]
-	if not done:
-		print "Unsatisfiable"
+	print "The clause set after calling the parser function is:"
+	print clauseSet
+	print "This is the solution"
+	for sol in pycosat.itersolve(clauseSet):
+		print sol
